99cd0fe76b1f85dea4e0fea54030c62adc57e5b5
evan <aspaasevan@gmail.com>
2020-03-09 14:56:57 -0700

    btm-content, contact and footer selectors done

--- js/index.js
+++ js/index.js
@@ -41,6 +41,7 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+
 // selector for nav links
 const navLinks = document.querySelectorAll('nav a');
 
@@ -51,6 +52,7 @@ navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
 navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
 navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
 navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+
 
 // selectors for cta
 const h1 = document.querySelector('.cta .cta-text h1');
@@ -65,3 +67,66 @@ button.textContent = siteContent["cta"]["button"];
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 
+// selectors for top content
+const topContentHeaders = document.querySelectorAll('.top-content .text-content h4');
+const featuresHeader = topContentHeaders[0];
+const aboutHeader = topContentHeaders[1];
+const topContentPara = document.querySelectorAll('.top-content .text-content p');
+const features = topContentPara[0];
+const about = topContentPara[1];
+const middleImg = document.getElementById('middle-img');
+
+// features header and paragraph
+featuresHeader.textContent = siteContent['main-content']['features-h4'];
+features.textContent = siteContent['main-content']['features-content'];
+
+// about header and paragraph
+aboutHeader.textContent = siteContent['main-content']['about-h4'];
+about.textContent = siteContent['main-content']['about-content'];
+
+// set src attribute of middle-img
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+// selectors for bottom content
+const bottomContentHeaders = document.querySelectorAll('.bottom-content .text-content h4');
+const servicesHeader = bottomContentHeaders[0];
+const productHeader = bottomContentHeaders[1];
+const visionHeader = bottomContentHeaders[2];
+
+const bottomContentPara = document.querySelectorAll('.bottom-content .text-content p');
+const services = bottomContentPara[0];
+const products = bottomContentPara[1];
+const visions = bottomContentPara[2];
+
+// services header and paragraph
+servicesHeader.textContent = siteContent['main-content']['services-h4'];
+services.textContent = siteContent['main-content']['services-content'];
+
+// product header and paragraph
+productHeader.textContent = siteContent['main-content']['product-h4'];
+products.textContent = siteContent['main-content']['product-content'];
+
+// vision header and paragraph
+visionHeader.textContent = siteContent['main-content']['vision-h4'];
+visions.textContent = siteContent['main-content']['vision-content'];
+
+
+// selectors for contact section
+const contactHeader = document.querySelector('.contact h4');
+const address = document.querySelector('.contact p');
+const phone = document.querySelector('.contact p:nth-of-type(even)');
+const email = document.querySelector('.contact p:last-child');
+
+// add text to contact section
+contactHeader.textContent = siteContent['contact']['contact-h4'];
+address.textContent = siteContent['contact']['address'];
+phone.textContent = siteContent['contact']['phone'];
+email.textContent = siteContent['contact']['email'];
+
+
+// selectors for footer
+const footer = document.querySelector('footer').firstChild;
+
+// add text to footer
+footer.textContent = siteContent['footer']['copyright'];
